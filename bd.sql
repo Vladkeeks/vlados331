1f5656a160b7aadef768bd2967c8cbf2f6224670
Vladkeeks <147250546+Vladkeeks@users.noreply.github.com>
2023-11-27 00:15:09 +0200

    Create bd.sql

---
+++ bd.sql
@@ -0,0 +1,16 @@
+SELECT name, verssion
+FROM Software;
+
+SELECT description, validity_date
+FROM licenses;
+
+
+SELECT computer_id, operating_system_id
+FROM Computers;
+
+SELECT user_id, name
+FROM Users;
+
+
+SELECT os_id, name
+FROM OperatingSystems;
